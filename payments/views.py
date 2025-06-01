3b5d5c903615c5b0c0c4d9450bc98ba49de3e6a3
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-01 23:39:21 +0300

    feat: check type of received info

--- payments/views.py
+++ payments/views.py
@@ -1,12 +1,20 @@
+import json
+
 from django.http import HttpRequest, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from payments.services import create_payment
 
 
+@csrf_exempt
 def handle_payment_request(request: HttpRequest):
     if request.method == "POST":
-        payment_data = request.POST
-        create_payment(**payment_data)
+        payment_data = json.loads(request.body)
+
+        try:
+            create_payment(**payment_data)
+        except TypeError:
+            return HttpResponse(status=400)
 
         return HttpResponse(status=200)
 
