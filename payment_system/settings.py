d0dd28c232b3f8cf4ba0e12934bbbecd6db1a6f2
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-01 17:32:48 +0300

    feat: register apps

--- payment_system/settings.py
+++ payment_system/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'orgs.apps.OrgsConfig',
+    'payments.apps.PaymentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
