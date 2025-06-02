0125398e82f30cfff36dd6ced8aa0af5aa52ba49
Daniil Lemeshko <gafret_kelp@outlook.com>
2025-06-02 12:35:56 +0300

    feat: load custom lookup

--- orgs/apps.py
+++ orgs/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class OrgsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "orgs"
+
+    def ready(self):
+        from django.db.models.fields import CharField
+        from django.db.models.functions import Length
+
+        CharField.register_lookup(Length, "length")
